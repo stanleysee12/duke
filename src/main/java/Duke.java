8a7a2e51377b5da3b7255808a59c8505ceb752aa
Stanley <E0895818@U.NUS.EDU>
2024-02-02 15:13:43 +0800

    end of week 3 progress
    Signed-off-by: Stanley <E0895818@U.NUS.EDU>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 
 
 public class Duke {
+
     public static void main(String[] args) {
         //Generated via https://patorjk.com/
         String logo =  "_______          __     _____ __________        __\n" +
@@ -15,6 +16,8 @@ public class Duke {
                         "What can I do for you?";
         String ending = "Bye. Hope to see you again soon";
         String userInput;
+        String [] arrayInput = new String[100];
+        int arrayIndex = 0 ;
 
         Scanner scGreeting = new Scanner(System.in);
 
@@ -25,11 +28,25 @@ public class Duke {
         while(true)
         {
             userInput = scGreeting.nextLine();
-            if(userInput.equalsIgnoreCase("exit"))
+            if(userInput.equalsIgnoreCase("bye"))
             {
+                System.out.println(ending);
                 System.exit(0);
             }
-            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~\n" + userInput);
+            else if(userInput.equalsIgnoreCase("list"))
+            {
+                for (int i = 0; i < arrayInput.length; i++) {
+                    if (arrayInput[i] == null)
+                    {
+                        //System.out.println("did i break");
+                        break;
+                    }
+                    System.out.println((i + 1) + ". " + arrayInput[i]);
+                }
+            }
+            arrayInput[arrayIndex] = userInput;
+            arrayIndex ++ ;
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~\n" + "added: " + userInput);
         }
 
     }
