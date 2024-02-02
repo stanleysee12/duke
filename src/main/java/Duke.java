031b088deeedf9bf0ff015c6d34fe1fcdae8ea8b
Stanley <E0895818@U.NUS.EDU>
2024-02-02 13:46:30 +0800

    Duke level 0 finished
    Signed-off-by: Stanley <E0895818@U.NUS.EDU>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //Generated via https://patorjk.com/
+        String logo =  "_______          __     _____ __________        __\n" +
+                "\\      \\   _____/  |_  /  _  \\______   \\ _____/  |_ \n" +
+                "/   |   \\ /  _ \\   __\\/  /_\\  \\|    |  _//  _ \\   __\\ \n" +
+                "/    |    (  <_> )  | /    |    \\   |   (  <_> )  |  \n" +
+                "\\____|__  /\\____/|__| \\____|__  /______  /\\____/|__|  \n";
+
+        String greeting = "Hello! I'm NotABot \n" +
+                        "What can I do for you?";
+        String ending = "Bye. Hope to see you again soon";
+                System.out.println("Hello from\n" + logo);
+                System.out.println(greeting);
+                System.out.println(ending);
     }
 }
