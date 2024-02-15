838486d7ffd6236ae7066288221a36a6a9f59289
Stanley <E0895818@U.NUS.EDU>
2024-02-15 12:22:34 +0800

    Level 3 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -16,7 +17,11 @@ public class Duke {
                         "What can I do for you?";
         String ending = "Bye. Hope to see you again soon";
         String userInput;
-        String [] arrayInput = new String[100];
+        String taskNo = null;
+        //String[] arrayInput = new String[100];
+        Task[ ] arrayInput = new Task[100];
+
+
         int arrayIndex = 0 ;
 
         Scanner scGreeting = new Scanner(System.in);
@@ -25,9 +30,22 @@ public class Duke {
         System.out.println(greeting);
 
 
+
+
         while(true)
         {
             userInput = scGreeting.nextLine();
+            //checks for unmark and mark first
+            if(userInput.length() > 2)
+            {
+                String[] temp = userInput.split(" ");
+                if((temp[0].equalsIgnoreCase("mark") || temp[0].equalsIgnoreCase("unmark")))
+                {
+                    userInput = temp[0];
+                    taskNo = temp[1];
+                }
+            }
+
             if(userInput.equalsIgnoreCase("bye"))
             {
                 System.out.println(ending);
@@ -35,18 +53,46 @@ public class Duke {
             }
             else if(userInput.equalsIgnoreCase("list"))
             {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < arrayInput.length; i++) {
                     if (arrayInput[i] == null)
                     {
-                        //System.out.println("did i break");
                         break;
                     }
-                    System.out.println((i + 1) + ". " + arrayInput[i]);
+
+                    System.out.printf("%d. [%s] %s%n", i + 1, arrayInput[i].getStatusIcon(),
+                            arrayInput[i].description);
+
                 }
             }
-            arrayInput[arrayIndex] = userInput;
-            arrayIndex ++ ;
-            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~\n" + "added: " + userInput);
+            else if((userInput.equalsIgnoreCase("mark")) || userInput.equalsIgnoreCase(("unmark")))
+            {
+                int number = Integer.parseInt(taskNo);
+                if(userInput.equalsIgnoreCase("mark"))
+                {
+                    arrayInput[number -1].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                }
+                else if(userInput.equalsIgnoreCase("unmark"))
+                {
+                    arrayInput[number -1].markAsNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                }
+
+                System.out.printf("[%s] %s%n", arrayInput[number-1].getStatusIcon(),
+                        arrayInput[number-1].description);
+            }
+            //Example
+            //Task t = new Task(userInput);
+            //t.markAsDone();
+            else {
+                arrayInput[arrayIndex] = new Task(userInput);
+                arrayIndex++;
+                //arrayInput[arrayIndex] = userInput;
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~\n" + "added: " + userInput);
+            }
+
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~\n");
         }
 
     }
